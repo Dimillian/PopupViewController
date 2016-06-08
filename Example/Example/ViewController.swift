9cc4d545d701adb17667d9495e8707613b9f209a
Thomas Ricouard <ricouard77@gmail.com>
2016-06-08 12:31:16 +0200

    Project setup

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Thomas Ricouard on 08/06/16.
+//  Copyright © 2016 Thomas Ricouard. All rights reserved.
+//
+
+import UIKit
+import PopupViewController
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func onStandardAlert(sender: AnyObject) {
+        let alert = PopupViewController(title: "Alert title", message: "Alert message, which can be very long and etc....")
+        alert.addAction(PopupAction(title: "Ok", type: .Positive, handler: nil))
+        presentViewController(alert, animated: true, completion: nil)
+    }
+
+    @IBAction func onCustomAlert(sender: AnyObject) {
+        var customizable = PopupViewController.Customizable()
+        customizable.titleColor = UIColor.blueColor()
+        customizable.actionColor = UIColor.redColor()
+        customizable.messagecolor = UIColor.brownColor()
+        customizable.messageFont = UIFont.boldSystemFontOfSize(22)
+        customizable.actionColor = UIColor.brownColor()
+        customizable.actionsHighlightColor = UIColor.redColor()
+        
+        let alert = PopupViewController(title: "Alert title",
+                                        message: "Aler message, which can be very long message and all that but nobody will ever read it.",
+                                        customizable: customizable)
+        alert.blurStyle = .ExtraLight
+        alert.addAction(PopupAction(title: "Ok", type: .Positive, handler: nil))
+        presentViewController(alert, animated: true, completion: nil)
+    }
+}
+
---
+++ PopupViewController/PopupViewController/PopupViewController.h
@@ -0,0 +1,19 @@
+//
+//  PopupViewController.h
+//  PopupViewController
+//
+//  Created by Thomas Ricouard on 08/06/16.
+//  Copyright © 2016 Thomas Ricouard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PopupViewController.
+FOUNDATION_EXPORT double PopupViewControllerVersionNumber;
+
+//! Project version string for PopupViewController.
+FOUNDATION_EXPORT const unsigned char PopupViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PopupViewController/PublicHeader.h>
+
+
